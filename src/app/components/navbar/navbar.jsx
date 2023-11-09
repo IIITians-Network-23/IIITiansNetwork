06d54b63deb48d040c5d234d55762079990b3208
DhairyaMajmudar <2022kuec2045@iiitkota.ac.in>
2023-11-09 12:42:02 +0530

    Adding Navbar

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -1,4 +1,34 @@
+"use client";
+
+import Logo from "../../assets/Logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import Avatar from "react-avatar";
+
+const links = [
+  { title: "IIITs", href: "/iiit_page" },
+  { title: "Alumini", href: "alumni_page" },
+  { title: "Blogs", href: "/" },
+  { title: "About Us", href: "/" },
+];
+
 const navbar = () => {
-  return <div className="bg-black w-[100px] h-[100px]">navbar is here</div>;
+  return (
+    <div className="bg-[#2E362B] flex flex-row justify-between items-center px-[100px] py-[15px]">
+      <div>
+        <Link href="/">
+          <Image src={Logo} alt="Logo" height={176} width={93} />
+        </Link>
+      </div>
+
+      <div className="flex flex-row gap-5 justify-between  items-center text-lg text-[#D5D0B2] ">
+        {links.map((link) => {
+          return <Link href={link.href}>{link.title}</Link>;
+        })}
+        <Avatar googleId="" size="30" round={true} />
+        {/* Do take reference from the link https://www.npmjs.com/package/react-avatar */}
+      </div>
+    </div>
+  );
 };
 export default navbar;
--- src/app/updates_page/page.jsx
+++ src/app/updates_page/page.jsx
@@ -2,8 +2,7 @@ import { Navbar, Footer } from "../components";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Navbar></Navbar>
-      <Footer></Footer>
+      
     </main>
   );
 }
